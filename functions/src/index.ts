41c2a8b116ca722f64b4f2df3446fa7abae6a232
cloudliner <cloudliner.jp@gmail.com>
2018-10-06 19:21:43 +0900

    add elasticsearch index and function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,3 +6,37 @@ import * as functions from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //   response.send("Hello from Firebase!");
 // });
+
+// const functions = require('firebase-functions');
+// const _ = require('lodash');
+// const request = require('request-promise');
+
+exports.indexCarsToElastic = functions.firestore
+  .document('/timeline/{itemId}')
+	.onWrite((change, context) => {
+		const itemData = change.after.data();
+		const itemId   = context.params.itemId;
+
+		console.log('Indexing timeline item ', itemId, itemData);
+
+		const elasticsearchFields = ['message'];
+		const elasticSearchConfig = functions.config().elasticsearch;
+		const elasticSearchUrl = elasticSearchConfig.url + 'timeline/item/' + itemId;
+		const elasticSearchMethod = itemData ? 'POST' : 'DELETE';
+
+		const elasticsearchRequest = {
+			method: elasticSearchMethod,
+			uri: elasticSearchUrl,
+			auth: {
+				username: elasticSearchConfig.username,
+				password: elasticSearchConfig.password,
+			},
+			body: _.pick(itemData, elasticsearchFields),
+			json: true
+		};
+
+		return request(elasticsearchRequest).then(response => {
+			console.log('Elasticsearch response', response);
+		})
+
+	});
