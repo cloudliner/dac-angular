eb0d9b7dc90b97754d1c414fc42eb5ccdccb982a
cloudliner <cloudliner.jp@gmail.com>
2018-10-08 19:04:01 +0900

    add search function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,7 @@ import * as functions from 'firebase-functions';
 import * as _ from 'lodash';
 import * as request from 'request-promise';
 
-exports.indexCarsToElastic = functions.firestore
+exports.indexTimelineToElastic = functions.firestore
   .document('/timeline/{itemId}')
 	.onWrite((change, context) => {
 		const itemData = change.after.data();
@@ -10,24 +10,49 @@ exports.indexCarsToElastic = functions.firestore
 
 		console.log('Indexing timeline item ', itemId, itemData);
 
-		const elasticsearchFields = ['message'];
+		const elasticSearchFields = ['message'];
 		const elasticSearchConfig = functions.config().elasticsearch;
 		const elasticSearchUrl = elasticSearchConfig.url + 'timeline/item/' + itemId;
 		const elasticSearchMethod = itemData ? 'POST' : 'DELETE';
 
-		const elasticsearchRequest = {
+		const elasticSearchRequest = {
 			method: elasticSearchMethod,
 			uri: elasticSearchUrl,
 			auth: {
 				username: elasticSearchConfig.username,
 				password: elasticSearchConfig.password,
 			},
-			body: _.pick(itemData, elasticsearchFields),
+			body: _.pick(itemData, elasticSearchFields),
 			json: true
 		};
 
-		return request(elasticsearchRequest).then(response => {
+		return request(elasticSearchRequest).then(response => {
 			console.log('Elasticsearch response', response);
 		})
 
-	});
+  });
+
+exports.searchTimelineElastic = functions.https.onRequest((req, res) => {
+  const keyword = req.query.keyword;
+
+  // const elasticSearchFields = ['message'];
+  const elasticSearchConfig = functions.config().elasticsearch;
+  const elasticSearchUrl = elasticSearchConfig.url + 'timeline/_search';
+  const elasticSearchMethod = 'POST';
+
+  const elasticSearchRequest = {
+    method: elasticSearchMethod,
+    uri: elasticSearchUrl,
+    auth: {
+      username: elasticSearchConfig.username,
+      password: elasticSearchConfig.password,
+    },
+    body: { query: { bool: { must: [{ term: { 'message' : keyword } }], must_not: [], should: [] }}, from: 0, size: 10},
+    json: true
+  };
+
+  request(elasticSearchRequest).then(response => {
+    console.log('ElasticSearch response', response);
+    res.send(response);
+  });
+});
